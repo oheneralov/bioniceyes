37abec9f04186ec73f098c6678f3363c5459e30c
alex <oheneralov@gmail.com>
2023-02-28 16:18:28 +0200

    updated 3d models

--- mobile/app/src/main/java/com/example/imagetoaudio/MainActivity.kt
+++ mobile/app/src/main/java/com/example/imagetoaudio/MainActivity.kt
@@ -1,53 +1,130 @@
 package com.example.imagetoaudio
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import coil.compose.rememberImagePainter
 import com.example.imagetoaudio.ui.theme.ImageToAudioTheme
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
+    private lateinit var photoUri: Uri
+    private var shouldShowPhoto: MutableState<Boolean> = mutableStateOf(false)
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.i("kilo", "Permission granted")
+            shouldShowCamera.value = true
+        } else {
+            Log.i("kilo", "Permission denied")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageToAudioTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            if (shouldShowCamera.value) {
+                CameraView(
+                    outputDirectory = outputDirectory,
+                    executor = cameraExecutor,
+                    onImageCaptured = ::handleImageCapture,
+                    onError = { Log.e("kilo", "View error:", it) }
+                )
+            }
+
+            if (shouldShowPhoto.value) {
+                Image(
+                    painter = rememberImagePainter(photoUri),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Column{
-        Button(onClick = {
-            println("Clicked")
-        }){
-            Text(text = "Submit")
+        requestCameraPermission()
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("kilo", "Permission previously granted")
+                shouldShowCamera.value = true
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i("kilo", "Show camera permissions dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun handleImageCapture(uri: Uri) {
+        Log.i("kilo", "Image captured: $uri")
+        shouldShowCamera.value = false
+
+        photoUri = uri
+        shouldShowPhoto.value = true
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
 
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 fun DefaultPreview() {
-    ImageToAudioTheme {
-        Greeting("Android")
-    }
+    Text("Hello World!!!")
+    outputDirectory = getOutputDirectory()
+    CameraView(
+        outputDirectory = outputDirectory,
+        executor = cameraExecutor,
+        onImageCaptured = ::handleImageCapture,
+        onError = { Log.e("kilo", "View error:", it) }
+    )
 }
+
